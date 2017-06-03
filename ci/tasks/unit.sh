93b6aa677e1c2ed776f579c8856c3ade018b3a9c
tkaburagi <tkaburagi@kabu-2.local>
2017-06-04 03:50:22 +0900

    test

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cd greeting-producer
+ 
+ ln -s `pwd`/../maven-cache ~/.m2/repository
+
 ./mvnw test -Dtest=GreetingProducerApplicationTests
---
+++ git.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git add -f .
+git commit -m $1
+git push
