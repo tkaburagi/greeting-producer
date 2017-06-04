55a7e986d40530cddb5121aed717ecd9df3e1e26
tkaburagi <tkaburagi@kabu-2.local>
2017-06-04 14:58:58 +0900

    test

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cd greeting-producer
 ./mvnw clean package
+
+ls -l target
+
 mv target/*.jar ../build/greeting-producer.jar
 #mv target/*.jar ../build/greeting-producer.jar
--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 cd greeting-producer
  
- ln -s `pwd`/../maven-cache ~/.m2/repository
-
 ./mvnw test -Dtest=GreetingProducerApplicationTests
